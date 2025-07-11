cbcf5287aacd0d7347cfcfd29dc88492f3ccbcf4
fallute <amfarrukh327@gmail.com>
2025-07-11 17:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,6 @@ def index():
     return "✅ GitHub Trigger Webhook is running!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))  # Render sets this automatically
+    app.run(host="0.0.0.0", port=port)
+
